9846c158719d5c855f41d4701eb8e9b1dbd15a55
Alberto García <albingarcia98@gmail.com>
2024-04-29 00:34:30 +0200

    primer test

---
+++ first_test/__manifest__.py
@@ -0,0 +1,17 @@
+
+{
+    'name': 'First test MFH',
+    'version': '16.0.1.0.0',
+    'author': 'Alberto García Gómez',
+    'maintainer': 'Alberto García Gómez',
+    'website': '',
+    'license': 'AGPL-3',
+    'category': 'Extra Tools',
+    'summary': 'Short summary.',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv'
+        'views/view.xml',
+    ],
+    'images': ['static/description/banner.png'],
+}
